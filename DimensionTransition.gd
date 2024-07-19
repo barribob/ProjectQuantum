8cadf3f337b42f1f43a200c369ad6b6ec90393b0
Barribob Development <32251924+miyo6032@users.noreply.github.com>
2024-07-19 08:35:04 -0600

    dimension transition

---
+++ DimensionTransition.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+@onready var dimensions: PanelContainer = %Dimensions
+
+func _ready() -> void:
+    hide()
+    dimensions.switch_dimension.connect(_switch_dimension)
+
+func _switch_dimension() -> void:
+    modulate.a = 1.0
+    show()
+    var tween = create_tween()
+    tween.tween_property(self, "modulate:a", 0.0, 0.2)
